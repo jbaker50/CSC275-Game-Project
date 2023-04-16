bc3fe80251c0cfe2559a78a281ac10e8c9816970
Jaylee Baker <88413877+jbaker50@users.noreply.github.com>
2023-04-16 15:51:39 -0500

    *

--- main.py
+++ main.py
@@ -41,10 +41,11 @@ exit_img = pygame.image.load('img/exit_btn.png').convert_alpha()
 restart_img = pygame.image.load('img/restart_btn.png').convert_alpha()
 settings_img = pygame.image.load('img/settings_btn.png').convert_alpha()
 #background
-pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
-pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
-mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
-sky_img = pygame.image.load('img/Background/sky_cloud.png').convert_alpha()
+layer0 = pygame.image.load('wotv_files_2/wotv_files/levels/swamp_resources/Background/Layers/0.png').convert_alpha()
+layer1 = pygame.image.load('wotv_files_2/wotv_files/levels/swamp_resources/Background/Layers/1.png').convert_alpha()
+layer2 = pygame.image.load('wotv_files_2/wotv_files/levels/swamp_resources/Background/Layers/2.png').convert_alpha()
+layer3 = pygame.image.load('wotv_files_2/wotv_files/levels/swamp_resources/Background/Layers/3.png').convert_alpha()
+layer4 = pygame.image.load('wotv_files_2/wotv_files/levels/swamp_resources/Background/Layers/4.png').convert_alpha()
 #store tiles in a list
 img_list = []
 for x in range(TILE_TYPES):
@@ -73,12 +74,13 @@ def draw_text(text, font, text_col, x, y):
 
 def draw_bg():
 	screen.fill(BG)
-	width = sky_img.get_width()
+	width = layer4.get_width()
 	for x in range(5):
-		screen.blit(sky_img, ((x * width) - bg_scroll * 0.5, 0))
-		screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
-		screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
-		screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
+		screen.blit(layer0, ((x * width) - bg_scroll * 0.5, 0))
+		screen.blit(layer1, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - layer0.get_height()))
+		#screen.blit(layer2, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - layer1.get_height()))
+		#screen.blit(layer3, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - layer2.get_height()))
+		#screen.blit(layer4, ((x * width) - bg_scroll * 0.9, SCREEN_HEIGHT - layer3.get_height()))
 
 
 #function to reset level
